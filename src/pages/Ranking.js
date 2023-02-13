bd1d329d885e77f34089c7fbadd6493374d87afa
matheuscpellanda <matheuscpellanda@hotmail.com>
2023-02-13 13:51:22 -0300

    Finalizando estilização e página ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,52 +1,62 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import Button from '../components/Button';
+import triviaIcon from '../images/logo.png';
 import './css/Ranking.css';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     const playerRanking = JSON.parse(localStorage.getItem('ranking') ?? '[]');
     return (
       <div className="ranking">
-        <h1 data-testid="ranking-title" className="rankingTitle">Ranking</h1>
-        <ol>
-          {
-            playerRanking.map((player, index) => (
-              <li key={ player.name }>
-                <img src={ player.image } alt={ player.name } />
-                <span data-testid={ `player-name-${index}` }>
-                  Jogador:
-                  { player.name }
-                </span>
-                <span data-testid={ `player-score-${index}` }>
-                  Pontuação:
-                  { player.score }
-                </span>
-              </li>
+        <div className="rankingForm">
 
-            ))
-          }
-        </ol>
+          <img src={ triviaIcon } alt="Ícon do Trívia" />
 
-        <Link to="/">
-          <button
+          <h1 data-testid="ranking-title" className="rankingTitle">Ranking</h1>
+
+          <ol>
+            {
+              playerRanking
+                .sort((a, b) => b.score - a.score)
+                .map((player, index) => (
+                  <li key={ player.name }>
+                    <img src={ player.image } alt={ player.name } />
+                    <span data-testid={ `player-name-${index}` }>
+                      Jogador:
+                      { player.name }
+                    </span>
+                    <span data-testid={ `player-score-${index}` }>
+                      Pontuação:
+                      { player.score }
+                    </span>
+                  </li>
+
+                ))
+            }
+          </ol>
+
+          <Button
             data-testid="btn-go-home"
+            text="Play Again"
             type="button"
-          >
-            Play Again
-          </button>
-        </Link>
+            handleClick={ () => { history.push('/'); } }
+            customClass="rankingPlayButton"
+          />
+
+        </div>
       </div>
     );
   }
 }
-// teste
 
-// Ranking.propTypes = {
-//   history: PropTypes.shape({
-//     push: PropTypes.func,
-//   }).isRequired,
-// };
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
